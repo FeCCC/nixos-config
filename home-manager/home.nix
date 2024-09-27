d24a657e8894eed79a239ce2221d7fff1b63d33a
FeCCC <railgun.long@foxmail.com>
2024-09-27 20:11:50 +0800

    zsh添加p10k主题配置

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -84,18 +84,22 @@
       la = "ls -a";
     };
     initExtra = ''
-      source ~/.p10k.zsh
-
       bindkey "^A" vi-beginning-of-line
       bindkey "^E" vi-end-of-line
     '';
     dotDir = ".config/zsh";
+    autosuggestion.enable = true;
     plugins = [
       {
         name = "powerlevel10k";
         src = pkgs.zsh-powerlevel10k;
         file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
       }
+      {
+          name = "powerlevel10k-config";
+          src = ./p10k-config;
+          file = "p10k.zsh";
+        }
     ];
   };
 
