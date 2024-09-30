c9a5f7535f48df058f48ea269017aa7ef9b74e56
FeCCC <railgun.long@foxmail.com>
2024-10-01 04:15:49 +0800

    sops添加keys

--- nixos/sops.nix
+++ nixos/sops.nix
@@ -7,10 +7,11 @@
     inputs.sops-nix.nixosModules.sops
   ];
   sops.defaultSopsFile = ../secrets/secrets.yaml;
-  sops.age.keyFile = "/root/.config/sops/age/keys.txt";
-  # Make sure that `/var/lib/sops` is owned by root and is not world-readable/writable
-  # sops.gnupg.home = "/var/lib/sops";
-  # disable importing host ssh keys
-  # sops.gnupg.sshKeyPaths = [];
+  sops.age.keyFile = "/var/lib/sops/age/keys";
+
   sops.secrets.miku_password.neededForUsers = true;
+  sops.secrets.sops_keys = {
+    path = "/var/lib/sops/age/keys";
+    owner = "root";
+  };
 }
