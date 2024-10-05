0187063fc7fd17d9079a61da3b5471a613be93c5
FeCCC <railgun.long@foxmail.com>
2024-10-06 00:45:20 +0800

    fix user sops

--- modules/home-manager/sops.nix
+++ modules/home-manager/sops.nix
@@ -7,8 +7,8 @@
   sops.defaultSopsFile = ../../secrets/secrets.yaml;
   sops.age.keyFile = "${config.xdg.configHome}/sops/age/keys";
 
-  sops.secrets.user_sops_keys = {
-    path = "${config.xdg.configHome}/sops/age/keys";
-    mode = "0600";
-  };
+  # sops.secrets.user_sops_keys = {
+  #   path = "${config.xdg.configHome}/sops/age/keys";
+  #   mode = "0600";
+  # };
 }
