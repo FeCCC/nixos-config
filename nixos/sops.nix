47a371be7ffb44b34c26cb73872854eca11326b3
FeCCC <railgun.long@foxmail.com>
2024-10-12 17:06:04 +0800

    添加root用户sops key脚本

--- nixos/sops.nix
+++ nixos/sops.nix
@@ -10,4 +10,14 @@
   sops.age.keyFile = "/var/lib/sops-keys";
 
   sops.secrets.miku_password.neededForUsers = true;
+
+  sops.secrets.sops_keys_init = {
+    path = "/root/.config/sops/sops_keys_init.sh";
+    mode = "0755";
+  };
+
+  sops.secrets.user_sops_keys_init = {
+    path = "/root/.config/sops/user_sops_keys_init.sh";
+    mode = "0755";
+  };
 }
