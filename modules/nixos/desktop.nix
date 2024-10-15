921c7a910a851b014ac58e40890119abba93e57e
FeCCC <railgun.long@foxmail.com>
2024-10-15 10:18:25 +0800

    添加桌面应用

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -12,7 +12,8 @@
     services.displayManager.sddm.wayland.enable = true;
     services.desktopManager.plasma6.enable = true;
     environment.systemPackages = with pkgs; [
-      # konsole
+      google-chrome
+      # citrix_workspace
     ];
   };
 }
