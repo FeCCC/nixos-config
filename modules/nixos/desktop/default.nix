f455389a8b9dd1829f77b12a1ceade8ac79584b8
FeCCC <railgun.long@foxmail.com>
2024-10-16 15:08:58 +0800

    调整desktop配置结构

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -1,17 +1,13 @@
 # Add your reusable NixOS modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
 # These should be stuff you would like to share with others, not your personal configurations.
-{
+{lib, ...}: {
   # List your module files here
   # my-module = import ./my-module.nix;
   imports = [
+    ./desktop.nix
     ./simple.nix
-    ./yggdrasil.nix
-    ./docker.nix
-    ./desktop
+    ./fonts.nix
   ];
 
-  environment.shellAliases = {
-    ll = "ls -l";
-    la = "ls -a";
-  };
+  options.my_config.desktop.enable = lib.mkEnableOption "use desktop";
 }
---
+++ modules/nixos/desktop/desktop.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  config = lib.mkIf config.my_config.desktop.enable {
+    services.xserver.enable = true;
+    services.displayManager.sddm.enable = true;
+    services.displayManager.sddm.wayland.enable = true;
+    services.desktopManager.plasma6.enable = true;
+
+    i18n = {
+      inputMethod = {
+        enabled = "fcitx5";
+        fcitx5 = {
+          addons = with pkgs; [
+            fcitx5-rime
+          ];
+          waylandFrontend = true;
+        };
+      };
+    };
+  };
+}
