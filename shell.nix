d7dfed200ee8fa5b33de1cfc6b725deb1be0c3eb
FeCCC <railgun.long@foxmail.com>
2024-12-09 18:32:15 +0800

    fix rust wayland lib

--- shell.nix
+++ shell.nix
@@ -45,7 +45,16 @@
       rustfmt
       rust-analyzer
       lldb
+      pkg-config
     ];
+
+    RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    LD_LIBRARY_PATH = with pkgs;
+      lib.makeLibraryPath [
+        libGL
+        libxkbcommon
+        wayland
+      ];
 
     shellHook = ''
       exec zsh
