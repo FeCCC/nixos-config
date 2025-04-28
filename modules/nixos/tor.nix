27912df2576dd1e127c696fab33befcdf15a06ac
FeCCC <railgun.long@foxmail.com>
2025-04-28 17:42:57 +0800

    add tor

---
+++ modules/nixos/tor.nix
@@ -0,0 +1,38 @@
+{
+  pkgs,
+  ...
+}:
+{
+  containers.tor = {
+    autoStart = true;
+    config =
+      { ... }:
+      {
+        networking = {
+          firewall = {
+            allowedTCPPorts = [
+              9050
+            ];
+            allowedUDPPorts = [
+              9050
+            ];
+          };
+        };
+        services = {
+          tor = {
+            enable = true;
+            client.enable = true;
+            settings = {
+              UseBridges = false;
+              ClientTransportPlugin = "obfs4,webtunnel exec ${pkgs.obfs4}/bin/lyrebird";
+              ClientUseIPv4 = true;
+              ClientUseIPv6 = true;
+              Bridge = [
+                # bridges
+              ];
+            };
+          };
+        };
+      };
+  };
+}
