ee3c45180af651959819b1ffa357b0e9ae4bbcbb
FeCCC <railgun.long@foxmail.com>
2025-05-14 20:10:27 +0800

    change syncthing dataDir

--- modules/nixos/syncthing.nix
+++ modules/nixos/syncthing.nix
@@ -7,7 +7,7 @@
   services.syncthing = {
     enable = true;
     user = "miku";
-    dataDir = "/home/miku/syncthing";
+    dataDir = "/home/miku";
     guiAddress = lib.mkIf (lib.hasAttr "wsl" config && config.wsl.enable) "127.0.0.1:8385";
   };
 
