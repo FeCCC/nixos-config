005476b7a68ac90442937d2dfc5745d95c81aa3d
FeCCC <railgun.long@foxmail.com>
2025-05-14 13:50:16 +0800

    change syncthing port on wsl

--- modules/nixos/syncthing.nix
+++ modules/nixos/syncthing.nix
@@ -1,8 +1,14 @@
-{ ... }:
+{
+  config,
+  lib,
+  ...
+}:
 {
   services.syncthing = {
     enable = true;
     user = "miku";
     dataDir = "/home/miku/syncthing";
+    guiAddress = lib.mkIf (lib.hasAttr "wsl" config && config.wsl.enable) "127.0.0.1:8385";
   };
+
 }
