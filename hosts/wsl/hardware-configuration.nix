3c0d63445b1655a97c0fc9627c94f11811e5a87e
FeCCC <railgun.long@foxmail.com>
2025-05-25 04:29:07 +0800

    add intel driver for wsl

--- hosts/wsl/hardware-configuration.nix
+++ hosts/wsl/hardware-configuration.nix
@@ -7,10 +7,17 @@
     fsType = "ext4";
   };
 
-  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.videoDrivers = [
+    "nvidia"
+    "modesetting"
+    "intel"
+    "fbdev"
+  ];
   hardware.graphics.enable = true;
-  hardware.nvidia.open = true;
-  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia = {
+    open = true;
+    modesetting.enable = true;
+  };
 
   # Set your system kind (needed for flakes)
   nixpkgs.hostPlatform = "x86_64-linux";
