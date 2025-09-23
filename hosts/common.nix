5a5cbad80e459cf543be843fa34b720c4fc31e51
FeCCC <railgun.long@foxmail.com>
2025-09-23 11:30:12 +0800

    use bbr kernel patch

--- hosts/common.nix
+++ hosts/common.nix
@@ -11,6 +11,17 @@
 
   boot.loader.grub.device = "nodev";
 
+  boot.kernelPatches = [
+    {
+      name = "bbr";
+      patch = null;
+      extraStructuredConfig = with pkgs.lib.kernel; {
+        TCP_CONG_BBR = yes; # enable BBR
+        DEFAULT_BBR = yes; # use it by default
+      };
+    }
+  ];
+
   my_os_config.docker.enable = true;
   my_os_config.desktop.enable = true;
 }
