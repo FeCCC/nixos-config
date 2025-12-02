5e6c1aa99e15a72122d5c3d45e5d4d5baabc51c8
FeCCC <railgun.long@foxmail.com>
2025-12-02 11:23:05 +0800

    update to 25.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -12,7 +12,7 @@
     nixpkgs-2305.url = "github:nixos/nixpkgs/nixos-23.05";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # wsl
@@ -35,7 +35,7 @@
 
     # nixvim
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-25.05";
+      url = "github:nix-community/nixvim/nixos-25.11";
       # If using a unstable channel you can use `url = "github:nix-community/nixvim`
       # If using a stable channel you can use `url = "github:nix-community/nixvim/nixos-<version>`
       inputs.nixpkgs.follows = "nixpkgs";
