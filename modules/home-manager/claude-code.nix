8dcd45f9181d791309f3b3fffe9809241dc6d090
FeCCC <railgun.long@foxmail.com>
2025-12-20 19:54:45 +0800

    claude-code使用ccr

--- modules/home-manager/claude-code.nix
+++ modules/home-manager/claude-code.nix
@@ -1,25 +1,55 @@
 {
+  lib,
   pkgs,
   config,
   ...
 }:
 {
+  home.packages = [ pkgs.claude-code-router ];
   programs.claude-code = {
     enable = true;
     package = pkgs.unstable.claude-code;
   };
+
   home.sessionVariables.CLAUDE_CONFIG_DIR = "${config.xdg.configHome}/claude";
-  sops.secrets.new_api_base_url_for_anthropic = { };
-  sops.templates.claude_code_settings = {
-    path = "${config.xdg.configHome}/claude/settings.json";
+  sops.secrets.new_api_base_url_for_ccr = { };
+  sops.templates.claude_code_router_settings = {
+    path = "${config.home.homeDirectory}/.claude-code-router/config.json";
     content = ''
       {
-          "env": {
-              "ANTHROPIC_BASE_URL": "${config.sops.placeholder.new_api_base_url_for_anthropic}",
-              "ANTHROPIC_MODEL": "deepseek/deepseek-v3.2",
-              "CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC": 1
-          },
-          "apiKeyHelper": "echo '${config.sops.placeholder.new_api_key}'"
+        "LOG": false,
+        "LOG_LEVEL": "debug",
+        "HOST": "127.0.0.1",
+        "PORT": 3456,
+        "APIKEY": "",
+        "API_TIMEOUT_MS": "600000",
+        "Providers": [
+          {
+            "name": "new_api",
+            "api_base_url": "${config.sops.placeholder.new_api_base_url_for_ccr}",
+            "api_key": "${config.sops.placeholder.new_api_key}",
+            "models": [
+              "deepseek/deepseek-v3.2",
+              "google/gemini-3-pro-preview",
+              "google/gemini-2.5-pro",
+              "google/gemini-3-flash-preview",
+              "gemini-3-flash-preview",
+              "gemini-3-pro-preview"
+            ],
+            "transformer": {
+              "use": []
+            }
+          }
+        ],
+        "Router": {
+          "default": "new_api,gemini-3-flash-preview",
+          "background": "new_api,gemini-3-flash-preview",
+          "think": "new_api,gemini-3-pro-preview",
+          "longContext": "new_api,gemini-3-flash-preview",
+          "longContextThreshold": 60000,
+          "webSearch": "",
+          "image": ""
+        },
       }
     '';
   };
