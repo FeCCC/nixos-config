8b1ec6d77e7182c34088d3f029ae56fc020eca5d
FeCCC <railgun.long@foxmail.com>
2025-12-21 03:42:13 +0800

    codex: 移除多余注释

--- modules/home-manager/codex.nix
+++ modules/home-manager/codex.nix
@@ -20,21 +20,14 @@
       model_provider = "new-api"
       model_context_window = 163840
       # model_context_window = 1048576
+      approval_policy = "on-request"
 
       [model_providers.new-api]
-      # Name of the provider that will be displayed in the Codex UI.
       name = "New Api"
-      # The path `/chat/completions` will be amended to this URL to make the POST
-      # request for the chat completions.
       base_url = "${config.sops.placeholder.new_api_base_url_for_openai}"
-      # If `env_key` is set, identifies an environment variable that must be set when
-      # using Codex with this provider. The value of the environment variable must be
-      # non-empty and will be used in the `Bearer TOKEN` HTTP header for the POST request.
       env_key = "NEWAPI_API_KEY"
       # Valid values for wire_api are "chat" and "responses". Defaults to "chat" if omitted.
       wire_api = "chat"
-      # The model decides when to escalate
-      approval_policy = "on-request"
 
       [features]
       web_search_request = true
@@ -44,4 +37,7 @@
     path = "${config.xdg.configHome}/codex/.env";
     content = "NEWAPI_API_KEY=${config.sops.placeholder.new_api_key}";
   };
+
+
+  programs.gemini-cli.enable = true;
 }
