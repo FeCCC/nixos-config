5dce0f4312171172cd1bc79c37136350ca081e2c
FeCCC <railgun.long@foxmail.com>
2025-12-28 02:56:36 +0800

    optimize wsl hardware config

--- hosts/wsl/hardware-configuration.nix
+++ hosts/wsl/hardware-configuration.nix
@@ -1,24 +1,28 @@
-# This is just an example, you should generate yours with nixos-generate-config and put it in here.
 {
-  # boot.loader.systemd-boot.enable = true;
+  config,
+  lib,
+  ...
+}:
+lib.mkMerge [
+  {
+    fileSystems."/" = {
+      device = "/dev/sda1";
+      fsType = "ext4";
+    };
 
-  fileSystems."/" = {
-    device = "/dev/sda1";
-    fsType = "ext4";
-  };
-
-  services.xserver.videoDrivers = [
-    "nvidia"
-    "modesetting"
-    "intel"
-    "fbdev"
-  ];
-  hardware.graphics.enable = true;
-  hardware.nvidia = {
-    open = true;
-    modesetting.enable = true;
-  };
-
-  # Set your system kind (needed for flakes)
-  nixpkgs.hostPlatform = "x86_64-linux";
-}
+    nixpkgs.hostPlatform = "x86_64-linux";
+  }
+  (lib.mkIf config.my_os_config.desktop.enable {
+    services.xserver.videoDrivers = [
+      "nvidia"
+      "modesetting"
+      "intel"
+      "fbdev"
+    ];
+    hardware.graphics.enable = true;
+    hardware.nvidia = {
+      open = true;
+      modesetting.enable = true;
+    };
+  })
+]
