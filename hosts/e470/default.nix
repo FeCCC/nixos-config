fec4da605eff56974c5558a7fd8d4ad2847eee84
FeCCC <railgun.long@foxmail.com>
2026-02-02 15:25:28 +0800

    feat: e470上添加安装程序，用于clawdbot

--- hosts/e470/default.nix
+++ hosts/e470/default.nix
@@ -9,7 +9,13 @@
     ./hardware-configuration.nix
   ];
 
+  environment.systemPackages = with pkgs; [
+    google-chrome
+    baidupcs-go
+  ];
+
   networking.hostName = "nixos-ThinkPad-E470";
+  networking.firewall.allowedTCPPorts = [ 18789 ];
 
   my_os_config.docker.enable = true;
   my_os_config.desktop.enable = true;
