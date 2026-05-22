f739166551457ef99af9b7e6464d16d2eacdbcfc
FeCCC <railgun.long@foxmail.com>
2026-05-22 23:53:11 +0800

    feat(aagl): add zenless zone zero launcher

--- modules/packs/desktop/nixos/aagl.nix
+++ modules/packs/desktop/nixos/aagl.nix
@@ -5,5 +5,6 @@
 {
   nix.settings = inputs.aagl.nixConfig; # Set up Cachix
   programs.honkers-railway-launcher.enable = true;
+  programs.sleepy-launcher.enable = true;
   aagl.enableNixpkgsReleaseBranchCheck = false;
 }
