dc06bbf235266809670e731471f75e55742ed838
FeCCC <railgun.long@foxmail.com>
2026-06-15 16:13:41 +0800

    chore: 修改docker主机的nfs挂载目录

--- hosts/docker/hardware-configuration.nix
+++ hosts/docker/hardware-configuration.nix
@@ -12,8 +12,8 @@
   services.rpcbind.enable = true; # needed for NFS
 
   fileSystems = {
-    "/mnt/NAS/docker" = {
-      device = "truenas.local:/mnt/NAS/docker";
+    "/mnt/NAS/appdata" = {
+      device = "truenas.local:/mnt/NAS/appdata/docker";
       fsType = "nfs";
     };
     "/mnt/NAS/docker-storage" = {
@@ -29,7 +29,7 @@
   systemd.services.docker = {
     # 'requires' 表示强依赖：如果挂载失败，Docker 不会启动
     requires = [
-      "mnt-NAS-docker.mount"
+      "mnt-NAS-appdata.mount"
       "mnt-NAS-docker\\x2dstorage.mount"
       "mnt-NAS-share.mount"
     ];
