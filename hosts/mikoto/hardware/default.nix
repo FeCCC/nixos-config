745b1ecc1ad09210052c20f890630f3868e64bb1
FeCCC <railgun.long@foxmail.com>
2026-06-18 09:25:09 +0800

    feat(mikoto): 支持docker容器使用nvidia显卡

--- hosts/mikoto/hardware/default.nix
+++ hosts/mikoto/hardware/default.nix
@@ -27,34 +27,6 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  # # nfs
-  # boot.supportedFilesystems = [ "nfs" ];
-  # services.rpcbind.enable = true; # needed for NFS
-  #
-  # fileSystems = {
-  #   "/mnt/NAS/appdata" = {
-  #     device = "truenas.local:/mnt/NAS/appdata/mikoto";
-  #     fsType = "nfs";
-  #   };
-  #   "/mnt/NAS/docker-storage" = {
-  #     device = "truenas.local:/mnt/NAS/docker-storage";
-  #     fsType = "nfs";
-  #   };
-  #   "/mnt/NAS/share" = {
-  #     device = "truenas.local:/mnt/NAS/share";
-  #     fsType = "nfs";
-  #   };
-  # };
-  #
-  # systemd.services.docker = {
-  #   # 'requires' 表示强依赖：如果挂载失败，Docker 不会启动
-  #   requires = [
-  #     "mnt-NAS-appdata.mount"
-  #     "mnt-NAS-docker\\x2dstorage.mount"
-  #     "mnt-NAS-share.mount"
-  #   ];
-  # };
-
   swapDevices = [ { device = "/dev/zvol/rpool/swap"; } ];
 
   #显卡
@@ -65,6 +37,8 @@
     nvidiaSettings = false;
     package = config.boot.kernelPackages.nvidiaPackages.legacy_580; # 580 是支持 V100 的最后一个版本
   };
+  hardware.opengl.driSupport32Bit = true;
+  virtualisation.docker.enableNvidia = true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
