11ec29237f4bdfcd7ed5090441d244480461db34
FeCCC <railgun.long@foxmail.com>
2026-08-24 21:54:42 +0800

    chore(docker): 防火墙放行本地ipv6地址

--- hosts/docker/default.nix
+++ hosts/docker/default.nix
@@ -32,6 +32,9 @@
   networking.firewall.allowedUDPPorts = [
     21116 # rustdesk ID 服务器
   ];
+  networking.firewall.extraInputRules = ''
+    ip6 saddr fd00::/8 accept
+  '';
   networking.wireless.enable = lib.mkForce false;
 
   my_config.docker.enable = true;
