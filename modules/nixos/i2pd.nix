b2007c8b3847175a46a10d4007a0d22c6ba1c1b9
FeCCC <railgun.long@foxmail.com>
2026-09-20 17:54:05 +0800

    refactor(nixos): tor/i2pd 去容器化为宿主服务，i2pd 激活时铺 reseed 证书

--- modules/nixos/i2pd.nix
+++ modules/nixos/i2pd.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 {
@@ -11,66 +12,53 @@
   };
 
   config = lib.mkIf config.my_config.i2pd.enable {
-    containers.i2pd = {
-      autoStart = true;
-      config =
-        { ... }:
-        {
-          networking.firewall = {
-            allowedTCPPorts = [
-              7070
-              4447
-              4444
-              7656
-              7654
-            ];
-            allowedUDPPorts = [
-              7070
-              4447
-              4444
-              7656
-              7654
-            ];
-          };
-          services.i2pd = {
-            enable = true;
-            logLevel = "info";
-            enableIPv4 = true;
-            enableIPv6 = true;
-            floodfill = true;
-            reseed.verify = true;
-            yggdrasil.enable = true;
-            proto = {
-              http = {
-                enable = true;
-                port = 7070;
-                strictHeaders = false;
-              };
-              httpProxy = {
-                enable = true;
-                port = 4444;
-                # outproxy = "http://exit.stormycloud.i2p";
-                outproxy = "http://outproxy.acetone.i2p";
-              };
-              socksProxy = {
-                enable = true;
-                port = 4447;
-                outproxyEnable = true;
-                outproxy = "localhost";
-                outproxyPort = 9050;
-              };
-              sam = {
-                enable = true;
-                port = 7656;
-              };
-              i2cp = {
-                enable = true;
-                port = 7654;
-              };
-            };
-          };
-          system.stateVersion = "24.11";
+    services.i2pd = {
+      enable = true;
+      logLevel = "info";
+      enableIPv4 = true;
+      enableIPv6 = true;
+      floodfill = true;
+      reseed.verify = true;
+      yggdrasil.enable = true;
+      proto = {
+        http = {
+          enable = true;
+          port = 7070;
+          strictHeaders = false;
         };
+        httpProxy = {
+          enable = true;
+          port = 4444;
+          # outproxy = "http://exit.stormycloud.i2p";
+          outproxy = "http://outproxy.acetone.i2p";
+        };
+        socksProxy = {
+          enable = true;
+          port = 4447;
+          outproxyEnable = true;
+          outproxy = "localhost";
+          outproxyPort = 9050;
+        };
+        sam = {
+          enable = true;
+          port = 7656;
+        };
+        i2cp = {
+          enable = true;
+          port = 7654;
+        };
+      };
+    };
+
+    # nixpkgs 模块无 extraConfig(#228182)、包也不安装 contrib/certificates,
+    # 而 reseed.verify=true 需要它们。switch 时直接铺进 datadir。
+    system.activationScripts.i2pd-certs = {
+      text = ''
+        mkdir -p /var/lib/i2pd/certificates
+        cp -r ${pkgs.i2pd.src}/contrib/certificates/. /var/lib/i2pd/certificates/
+        chown -R i2pd:i2pd /var/lib/i2pd/certificates
+      '';
+      deps = [ ];
     };
   };
 }
--- modules/nixos/tor.nix
+++ modules/nixos/tor.nix
@@ -12,38 +12,18 @@
   };
 
   config = lib.mkIf config.my_config.tor.enable {
-    containers.tor = {
-      autoStart = true;
-      config =
-        { ... }:
-        {
-          networking = {
-            firewall = {
-              allowedTCPPorts = [
-                9050
-              ];
-              allowedUDPPorts = [
-                9050
-              ];
-            };
-          };
-          services = {
-            tor = {
-              enable = true;
-              client.enable = true;
-              settings = {
-                UseBridges = false;
-                ClientTransportPlugin = "obfs4,webtunnel exec ${pkgs.obfs4}/bin/lyrebird";
-                ClientUseIPv4 = true;
-                ClientUseIPv6 = true;
-                Bridge = [
-                  # bridges
-                ];
-              };
-            };
-          };
-          system.stateVersion = "24.11";
-        };
+    services.tor = {
+      enable = true;
+      client.enable = true;
+      settings = {
+        UseBridges = false;
+        ClientTransportPlugin = "obfs4,webtunnel exec ${pkgs.obfs4}/bin/lyrebird";
+        ClientUseIPv4 = true;
+        ClientUseIPv6 = true;
+        Bridge = [
+          # bridges
+        ];
+      };
     };
   };
 }
